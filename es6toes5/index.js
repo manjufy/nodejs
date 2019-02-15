2ccb891bb088af5afa75a419a87e6e2efa0f3390
Manjunath Reddy <manju16832003@gmail.com>
2019-02-15 12:59:16 +0800

    Default export example

--- es6toes5/index.js
+++ es6toes5/index.js
@@ -1,4 +1,7 @@
 import userService from './user'
-
+import defaultEx from './default-example'
 const user = userService()
 console.log('User',  user.getUser())
+
+console.log(defaultEx.getCookies());
+console.log(defaultEx.getMePizza());
